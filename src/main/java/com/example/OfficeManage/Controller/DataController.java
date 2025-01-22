b9c1f6fa56b894f4a9abfab653fae2a9c2941d5d
Shubham Kumar <shubhammisra800@gmail.com>
2025-01-23 01:29:44 +0530

    Hello

--- src/main/java/com/example/OfficeManage/Controller/DataController.java
+++ src/main/java/com/example/OfficeManage/Controller/DataController.java
@@ -16,7 +16,7 @@ import com.example.OfficeManage.Service.DataService;
 
 @RestController
 @RequestMapping("/data")
-@CrossOrigin("https://skmprofile.netlify.app/")
+@CrossOrigin("https://skmprofile.netlify.app")
 public class DataController {
 	@Autowired
 	private DataService dsr;
