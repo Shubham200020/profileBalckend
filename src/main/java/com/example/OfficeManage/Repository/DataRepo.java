3d72b4087ccf1986ac0d75a74f832d822f827297
Shubham Kumar <shubhammisra800@gmail.com>
2025-02-04 10:11:45 +0530

    Something

--- src/main/java/com/example/OfficeManage/Repository/DataRepo.java
+++ src/main/java/com/example/OfficeManage/Repository/DataRepo.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 
 import com.example.OfficeManage.POJO.DataSkm;
 @Repository
-public interface DataRepo extends MongoRepository<DataSkm, Integer> {
+public interface DataRepo extends MongoRepository<DataSkm, String> {
 
 }
